7284f8b183276ee1c84ce583664e161dc02d86bf
Abhishek D S <abhishekds94@gmail.com>
2019-07-04 19:28:25 +0530

    New Files for DB and Admin

---
+++ final.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+	<head>
+	<meta charset="utf-8" />
+	<title>Quizzer</title>
+	<link rel="stylesheet" href="css/style.css" type="text/css" />
+</head>
+<body>
+	<header>
+		<div class="container">
+			<h1> Quizzer </h1>
+		</div>
+	</header>
+	<main>
+		<div class="container">
+			<h2> You're Done! </h2>
+			<p>Congrats! Your final score is 5</p>
+			<a href="question.php?n=1" class="startquiz">Take Again</a>
+		</div>
+	</main>
+	<footer>
+		<div class="container">
+			Copyright &copy; 2019, Quizzer
+		</div>
+	</footer>
+</body>
+</html>
